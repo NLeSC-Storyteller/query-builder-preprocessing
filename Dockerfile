ee4b3590a1a97798654c8cfc03f56869b9f9be60
maartenvm <maartenvm@gmail.com>
2017-03-14 16:36:37 +0100

    some tweaks to names of data files

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ VOLUME /data
 
 WORKDIR /src/query-builder-preprocessing
 COPY run_docker_stack.sh .
+RUN chmod +x run_docker_stack.sh
 
 EXPOSE 4567
 CMD ["./run_docker_stack.sh", "/data"]
--- create_all.sh
+++ create_all.sh
@@ -5,7 +5,7 @@ DB=$2
 
 python3 $DIR/src/create_sqlite.py --input $DATA_DIR/light.json            --name $DB --tablename light
 python3 $DIR/src/create_sqlite.py --input $DATA_DIR/dark.json             --name $DB --tablename dark
-python3 $DIR/src/create_sqlite.py --input $DATA_DIR/concepts.json         --name $DB --tablename concepts
+python3 $DIR/src/create_sqlite.py --input $DATA_DIR/concept.json          --name $DB --tablename concepts
 
 python3 $DIR/src/create_sqlite.py --input $DATA_DIR/events.json           --name $DB --tablename events
 python3 $DIR/src/create_sqlite.py --input $DATA_DIR/authors.json          --name $DB --tablename authors
